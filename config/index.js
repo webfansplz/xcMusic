dba35a04176156df76276babe7983668a71a2f90
csh <308241863@qq.com>
2017-05-17 17:18:04 +0800

    repair vux-loader no install bug

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 8099,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
